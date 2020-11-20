408f97af86b17988de85cb51209d0339014e4dcb
tallulabellm <tmizra@cisco.com>
2020-11-20 18:52:14 +0000

    disk caches now output to directory with func name

--- diskcache.cpp
+++ diskcache.cpp
@@ -8,8 +8,13 @@ using std::function;
 using std::string;
 
 template <typename Ret, typename ...Args> 
+string DiskCache<Ret,Args...>::makePathForKey(string key) {
+  return this->outputPath + key + ".txt";
+}
+
+template <typename Ret, typename ...Args> 
 std::optional<Ret> DiskCache<Ret,Args...>::get(Args... args) {
-  std::ifstream inFile(this->key(args...)+".txt");
+  std::ifstream inFile(makePathForKey(this->key(args...)));
   if (!inFile) {
       std::cout << "Unable to open file" << std::endl;
       return nullopt;
@@ -24,7 +29,7 @@ std::optional<Ret> DiskCache<Ret,Args...>::get(Args... args) {
 template <typename Ret, typename ...Args> 
 void DiskCache<Ret,Args...>::put(Args... args, Ret value)  {
   std::cout << "keying for put: " << this->key(args...) << std::endl;
-  std::ofstream myfile(this->key(args...)+".txt");
+  std::ofstream myfile(makePathForKey(this->key(args...)));
   myfile << this->pickle(value);
   myfile.close();
 }
--- diskcache.hpp
+++ diskcache.hpp
@@ -4,14 +4,19 @@
 template <typename Ret, typename ...Args> 
 class DiskCache : public Cache<Ret,Args...>{
   public:
-    DiskCache(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string))
+    DiskCache(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string funcName)
     {
       this->key = key;
       this->pickle = pickle;
       this->unpickle = unpickle;
+      this->funcName = funcName;
+      this->outputPath = "./"+funcName+"/";
     }
     std::optional<Ret> get(Args... args);
     void put(Args... args, Ret value);
+  private:
+    string makePathForKey(string key);
+    string outputPath;
 };
 
 #if !defined(DISKCACHE_H_FILE)
