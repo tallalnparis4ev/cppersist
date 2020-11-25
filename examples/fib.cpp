2015da14c62f3f0bf316faa73a2ab919468f894d
tallulabellm <tmizra@cisco.com>
2020-11-25 16:08:30 +0000

    made examples dir, and prime factors example

--- examples/fib.cpp
+++ examples/fib.cpp
@@ -1,4 +1,4 @@
-#include "persister.hpp"
+#include "../persister.hpp"
 #include <iostream>
 //Fibonacci Sequence Example
 class FibonacciSolver: public PersistentMemoizable<int, int>{
@@ -22,13 +22,13 @@ string intostr(int x){
 string keymaker(int x){
   return std::to_string(x);
 }
+using namespace std;
 int main(){
   FibonacciSolver fibonacciSolver;
-  // PersistentMemoized memoizedFib = Persister::getLocalMemoizedObj<FibonacciSolver>(fibonacciSolver,keymaker,intostr,strtoi);
-  PersistentMemoized memoizedFib = Persister::getMongoMemoizedObj<FibonacciSolver>(fibonacciSolver,keymaker,intostr,strtoi);
-  PersistentMemoized memoizedFib2 = Persister::getMongoMemoizedObj<FibonacciSolver>(fibonacciSolver,keymaker,intostr,strtoi);
+  PersistentMemoized memoizedFib = Persister::getLocalMemoizedObj<FibonacciSolver>(fibonacciSolver,keymaker,intostr,strtoi,"fib");
+  // PersistentMemoized memoizedFib = 
+    // Persister::getMongoMemoizedObj<FibonacciSolver>(fibonacciSolver,keymaker,intostr,strtoi,"https://tm214.host.cs.st-andrews.ac.uk");
   int z = memoizedFib(3);
-  int y = memoizedFib2(3);
   std::cout << z << std::endl;
-  std::cout << y << std::endl;
+  cout << typeid(memoizedFib).name() << endl;
 }
---
+++ examples/primes.cpp
@@ -0,0 +1,61 @@
+#include "../persister.hpp"
+#include <iostream>
+#include <list> 
+#include <iterator> 
+using std::list;
+using namespace std;
+// Prime Factors Example
+class PrimeFactorizer: public PersistentMemoizable<list<int>, int>{
+  public:
+    list<int> solve(int n) override {
+      list <int> primeFactors; 
+      std::cout << "Original " << n << std::endl;
+      int curPrime = 2;
+      while(n >= (curPrime*curPrime)){
+        if(n%curPrime == 0){
+          primeFactors.push_back(curPrime);
+          n = n/curPrime;
+        }
+        else curPrime++;
+      }
+      primeFactors.push_back(n);
+      return primeFactors;
+    }
+};
+
+list<int> intListStrToList(string primeString){
+  list <int> primeFactors; 
+  string curPrime = "";
+  for(char& cur : primeString) {
+    if(cur != ' '){
+      curPrime += cur;
+    }
+    else{
+      primeFactors.push_back(std::stoi(curPrime));
+      curPrime = "";
+    }
+  }
+  return primeFactors;
+}
+string listIntsToString(list<int> primes){
+  string ret = "";
+  for(int prime : primes){
+    ret += (std::to_string(prime) + " ");
+  }
+  return ret;
+}
+
+string keymaker(int n){
+  return std::to_string(n);
+}
+
+int main(){
+  PrimeFactorizer primeFactorizer;
+  PersistentMemoized memoizedPrimes = 
+    Persister::getLocalMemoizedObj<PrimeFactorizer>
+      (primeFactorizer,keymaker,listIntsToString,intListStrToList,"primes");
+  // PersistentMemoized memoizedFib = 
+    // Persister::getMongoMemoizedObj<FibonacciSolver>(fibonacciSolver,keymaker,intostr,strtoi,"https://tm214.host.cs.st-andrews.ac.uk");
+  list<int> z = memoizedPrimes(20);
+  std::cout << listIntsToString(z) << std::endl;
+}
