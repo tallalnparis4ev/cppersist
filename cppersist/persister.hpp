1cd2f6a6f9d3e533a2842178626f3b1da0c972bb
tallulabellm <tmizra@cisco.com>
2021-01-02 23:10:46 +0000

    Make all memoize fns consistentn

--- cppersist/persister.hpp
+++ cppersist/persister.hpp
@@ -26,19 +26,13 @@ class PersistentMemoizable
 template<typename T, typename Ret, typename ...Args>
 PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string));
 
-template<typename T, typename Ret, typename... Args, 
-  typename std::enable_if<std::is_base_of<PersistentMemoizable<Ret, Args...>, T>::value>::type* 
-    = nullptr>
+template<typename T, typename Ret, typename... Args>
 PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string funcName);
 
-template<typename T, typename Ret, typename... Args, 
-  typename std::enable_if<std::is_base_of<PersistentMemoizable<Ret, Args...>, T>::value>::type* 
-    = nullptr>
+template<typename T, typename Ret, typename... Args>
 PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL);
 
-template<typename T, typename Ret, typename... Args, 
-  typename std::enable_if<std::is_base_of<PersistentMemoizable<Ret, Args...>, T>::value>::type* 
-    = nullptr>
+template<typename T, typename Ret, typename... Args>
 PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL, string funcName);
 
 #include "persister.cpp"
