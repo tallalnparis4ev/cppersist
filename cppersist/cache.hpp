5ee5a414d7b85ad8007534890a39717aa9a165de
tallulabellm <tmizra@cisco.com>
2021-01-04 21:12:34 +0000

    changed cache interface - uses references instead of values...reduces copying

--- cppersist/cache.hpp
+++ cppersist/cache.hpp
@@ -7,12 +7,13 @@ template <typename Ret, typename ...Args>
 class Cache
 {
   public:
-    virtual std::optional<Ret> get(Args...) = 0;
-    virtual void put(Args..., Ret) = 0;
+    virtual std::optional<Ret> get(const Args&...) = 0;
+    virtual void put(const Args&..., const Ret&) = 0;
+    virtual ~Cache() {};
   protected:
-    std::string (*key)(Args...);
-    std::string (*pickle)(Ret);
-    Ret (*unpickle)(std::string);
+    std::string (*key)(const Args&...);
+    std::string (*pickle)(const Ret&);
+    Ret (*unpickle)(const std::string&);
     std::string funcName;
 };
 
