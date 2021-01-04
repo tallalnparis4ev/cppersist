c94280797774357ef7b4f90ab88d7d04f513a1f1
tallulabellm <tmizra@cisco.com>
2021-01-04 21:20:07 +0000

    changed local cache header file to use const refs

--- cppersist/local.hpp
+++ cppersist/local.hpp
@@ -4,10 +4,10 @@
 #include "persister.hpp"
 
 template<typename T, typename Ret, typename ...Args>
-PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string));
+PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(const Args&...),string (*pickle)(const Ret&),Ret (*unpickle)(const string&));
 
 template<typename T, typename Ret, typename... Args>
-PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string funcName);
+PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(const Args&...),string (*pickle)(const Ret&),Ret (*unpickle)(const string&));
 
 #include "local.cpp"
 #endif
