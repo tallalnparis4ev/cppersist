3909503055b21c6579d26a2d9dedb32017aef52d
tallulabellm <tmizra@cisco.com>
2021-01-05 20:45:04 +0000

    mongo hybrid cache

--- cppersist/mongo.cpp
+++ cppersist/mongo.cpp
@@ -1,4 +1,5 @@
 #include "cacheimpl/mongodbcache.hpp"
+#include "cacheimpl/regcache.hpp"
 #include <iostream>
 #include <string>
 
@@ -23,5 +24,35 @@ PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(string (*key)(const Args&.
   
   MongoDBCache<Ret,Args...>* mongoCache = new MongoDBCache<Ret,Args...>(key,pickle,unpickle,dbURL,funcName);
   PersistentMemoized<T,Ret,Args...> memoized(mongoCache);
+  return memoized;
+}
+
+template<typename T, typename Ret, typename... Args>
+PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType type,string (*key)(const Args&...),string (*pickle)(const Ret&),Ret (*unpickle)(const string&), string dbURL){
+  static_assert(std::is_base_of<PersistentMemoizable<Ret,Args...>, T>::value, 
+    "Must Memoize a class that inherits from PersistentMemoizable");
+  
+  string funcName = typeid(T).name();
+  std::cout << "ALERT: No function name passed, using " << funcName << " as the function name instead!" << std::endl;
+  MemCache<Ret,Args...>* primaryCache = NULL;
+  switch(type){
+    default: primaryCache = new RegCache<Ret,Args...>(key,pickle,unpickle);
+  }
+  MongoDBCache<Ret,Args...>* mongoCache = new MongoDBCache<Ret,Args...>(key,pickle,unpickle,dbURL,funcName);
+  PersistentMemoized<T,Ret,Args...> memoized(primaryCache,mongoCache);
+  return memoized;
+}
+
+template<typename T, typename Ret, typename... Args>
+PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType type,string (*key)(const Args&...),string (*pickle)(const Ret&),Ret (*unpickle)(const string&), string dbURL, string funcName){
+  static_assert(std::is_base_of<PersistentMemoizable<Ret,Args...>, T>::value, 
+    "Must Memoize a class that inherits from PersistentMemoizable");
+  
+  MemCache<Ret,Args...>* primaryCache = NULL;
+  switch(type){
+    default: primaryCache = new RegCache<Ret,Args...>(key,pickle,unpickle);
+  }
+  MongoDBCache<Ret,Args...>* mongoCache = new MongoDBCache<Ret,Args...>(key,pickle,unpickle,dbURL,funcName);
+  PersistentMemoized<T,Ret,Args...> memoized(primaryCache,mongoCache);
   return memoized;
 }
--- cppersist/mongo.hpp
+++ cppersist/mongo.hpp
@@ -9,5 +9,11 @@ PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(string (*key)(const Args&.
 template<typename T, typename Ret, typename... Args>
 PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(string (*key)(const Args&...),string (*pickle)(const Ret&),Ret (*unpickle)(const string&), string dbURL, string funcName);
 
+template<typename T, typename Ret, typename... Args>
+PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType type,string (*key)(const Args&...),string (*pickle)(const Ret&),Ret (*unpickle)(const string&), string dbURL);
+
+template<typename T, typename Ret, typename... Args>
+PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType type,string (*key)(const Args&...),string (*pickle)(const Ret&),Ret (*unpickle)(const string&), string dbURL, string funcName);
+
 #include "mongo.cpp"
 #endif
