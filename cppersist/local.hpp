92674ae1694d0ef5f211023d39c1a1b7cb597f9a
tallulabellm <tmizra@cisco.com>
2021-01-16 21:26:00 +0000

    comments, and doxygen config added

--- cppersist/local.hpp
+++ cppersist/local.hpp
@@ -3,17 +3,80 @@
 
 #include "memoization/persister.hpp"
 namespace cpst{
+  /**
+   * \defgroup PersistLocal Persist Local Filesystem
+   * @{
+   */
+
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is the local file system.
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   */ 
   template<typename T, typename Ret, typename ...Args>
   PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string));
 
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is the local file system. 
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   * @param funcName - files will be stored at "./cppersist/{funcName}", if 
+   * funcName is not specified typeid(T).name() is used instead.
+   */ 
   template<typename T, typename Ret, typename... Args>
   PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string funcName);
 
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is the local file system. 
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param primaryCache - this will initialise the specified memory cache and use
+   * it as the primary cache.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   */ 
   template<typename T, typename Ret, typename... Args>
   PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(MemCacheType primaryCache,string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string));
 
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is the local file system. 
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param primaryCache - this will initialise the specified memory cache and use
+   * it as the primary cache.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   * @param funcName - files will be stored at "./cppersist/{funcName}", if 
+   * funcName is not specified typeid(T).name() is used instead.
+   */ 
   template<typename T, typename Ret, typename... Args>
   PersistentMemoized<T,Ret,Args...> getLocalMemoizedObj(MemCacheType primaryCache,string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string funcName);
+
+  /**@}*/
 }
 #include "local.cpp"
 #endif
--- cppersist/mongo.hpp
+++ cppersist/mongo.hpp
@@ -3,17 +3,86 @@
 
 #include "memoization/persister.hpp"
 namespace cpst{
+  /**
+   * \defgroup PersistMongo Persist MongoDB
+   * @{
+   */
+
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is mongoDB.
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   * @param dbUrl - this corresponds to the URL of the mongoDB server.
+   */ 
   template<typename T, typename Ret, typename... Args>
   PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL);
 
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is mongoDB.
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   * @param dbUrl - this corresponds to the URL of the mongoDB server.
+   * @param funcName - this corresponds to which collection in the 'cppersist'
+   * database results should be persisted. If this is not passed in, the name
+   * typeid(T).name() is used instead.
+   */ 
   template<typename T, typename Ret, typename... Args>
   PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL, string funcName);
 
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is mongoDB.
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param primaryCache - this will initialise the specified memory cache and use
+   * it as the primary cache.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   * @param dbUrl - this corresponds to the URL of the mongoDB server.
+   */ 
   template<typename T, typename Ret, typename... Args>
-  PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType type,string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL);
+  PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType primaryCache,string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL);
 
+  /**
+   * Creates an object, of type T, that has persistent memoization applied 
+   * to T's solve function. Here, the storage medium for persistent memoization
+   * is mongoDB.
+   * @tparam T - the class which the user wants to instantiate an object of,
+   * with a memoized version of the solve function.
+   * @param primaryCache - this will initialise the specified memory cache and use
+   * it as the primary cache.
+   * @param key - this function should return a string representation for a 
+   * given argument to the solve function. 
+   * @param pickle - this function should return a string representation
+   * for any return of the solve function.
+   * @param unpickle - this function is the inverse of pickle.
+   * @param dbUrl - this corresponds to the URL of the mongoDB server.
+   * @param funcName - this corresponds to which collection in the 'cppersist'
+   * database results should be persisted. If this is not passed in, 
+   * typeid(T).name() is used instead.
+   */ 
   template<typename T, typename Ret, typename... Args>
-  PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType type,string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL, string funcName);
+  PersistentMemoized<T,Ret,Args...> getMongoMemoizedObj(MemCacheType primaryCache,string (*key)(Args...),string (*pickle)(Ret),Ret (*unpickle)(string), string dbURL, string funcName);
+
+  /**@}*/
 }
 #include "mongo.cpp"
 #endif
