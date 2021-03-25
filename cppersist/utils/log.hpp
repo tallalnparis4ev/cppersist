5c1e430f5c070da13b81943b2cd1714ca1974f9c
tallalnparis4ev <tm214@st-andrews.ac.uk>
2021-03-25 16:29:27 +0000

    forgot to commit this, boolean flag that indicates if should log

--- cppersist/utils/log.hpp
+++ cppersist/utils/log.hpp
@@ -4,25 +4,30 @@
 #include <iostream>
 using namespace std;
 namespace cpst {
+bool shouldLog = false;
 template <typename T>
 void print(T value) {
+  if(!shouldLog) return;
   cout << value << ", ";
 }
 
 template <typename First, typename... Rest>
 void print(First firstValue, Rest... rest) {
+  if(!shouldLog) return;
   print(firstValue);
   print(rest...);
 }
 
 template <typename First, typename... Rest>
 void log(First firstValue, Rest... rest) {
+  if(!shouldLog) return;
   print(firstValue, rest...);
   cout << endl;
 }
 
 template <typename Single>
 void logOne(Single singleValue) {
+  if(!shouldLog) return;
   cout << singleValue << endl;
 }
 }  // namespace cpst
