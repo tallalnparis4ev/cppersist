defb31b59c6bbd3f084b030eeb1ed005b0fb5aa6
tallalnparis4ev <tm214@st-andrews.ac.uk>
2021-04-04 15:14:54 +0100

    forgot to add to namespace cpst

--- cppersist/memoization/memcachetypes.hpp
+++ cppersist/memoization/memcachetypes.hpp
@@ -1,19 +1,21 @@
 #if !defined(MEM_CACHE_TYPES)
 #define MEM_CACHE_TYPES
-/**
- * @file
- * Defines the different memory caches that can be used
- */
-enum class MemCacheType {
-  /** A memory cache of unbounded size */
-  REGULAR,
+namespace cpst{
   /**
-   * A memory cache of maximum size = 1000 entries, by default.
-   * The eviction policy is least recently used. The size of this can
-   * be changed by calling setMemoryCache(LRU, desired size).
+   * @file
+   * Defines the different memory caches that can be used
    */
-  LRU,
-  /** A memory cache of maximum size = one */
-  ONE
-};
+  enum class MemCacheType {
+    /** A memory cache of unbounded size */
+    REGULAR,
+    /**
+     * A memory cache of maximum size = 1000 entries, by default.
+     * The eviction policy is least recently used. The size of this can
+     * be changed by calling setMemoryCache(LRU, desired size).
+     */
+    LRU,
+    /** A memory cache of maximum size = one */
+    ONE
+  };
+}
 #endif  // MEM_CACHE_TYPES
