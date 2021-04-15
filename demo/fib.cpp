b39ae9246c92cdbad45e23a09bfa58136e4ac522
tallalnparis4ev <tm214@st-andrews.ac.uk>
2021-04-15 02:48:26 +0100

    better example

--- demo/fib.cpp
+++ demo/fib.cpp
@@ -1,41 +1,37 @@
-#include "cppersist/local.hpp"
-// #include "cppersist/mongo.hpp"
-using namespace cpst;
-using namespace std;
-class FibSolver : Memoizable<int, int>{
+#include <cppersist/local.hpp> //if you need to memoize with your filesystem
+// #include <cppersist/mongo.hpp> //if you need to memoize with MongoDB
+#include <cppersist/memory.hpp> //if you need to memoize in-memory
+#include <iostream>
+
+//The class to be memoized - must implement a solve method
+class FibonacciSolver: public cpst::Memoizable<int, int>{
   public:
     int solve(int n) override {
-      std::cout << "Original" << std::endl;
-      if(n==0) return 0;
-      if(n==1) return 1;
+      if(n<=1) return n;
       return solve(n-1) + solve(n-2);
-    };
+    }
 };
 
-int strtoi(string x){
+//Pickle/serialize function
+std::string serialize(int x){
+  return std::to_string(x);
+}
+//Unpickle/deserialize function
+int deserialize(std::string x){
   return std::stoi(x);
 }
-string intostr(int x){
+//Key function
+std::string keymaker(int x){
   return std::to_string(x);
 }
 
-string fibKeyMaker(int x){
-  return std::to_string(x);
-}
+int main(int argc, char** argv) {
+  cpst::PersistentMemoized memoizedFib = cpst::getLocalMemoizedObj<FibonacciSolver>(keymaker,serialize,deserialize); //disk cache
+  std::cout << memoizedFib(3) << std::endl;
 
-string hashKey(string key){
-  return key;
-}
-int main(int argc, char const *argv[])
-{
-  auto localFib = getLocalMemoizedObj<FibSolver>(
-    fibKeyMaker,intostr,strtoi,"fib",hashKey
-  );
+  cpst::Memoized memoizedInMemoryFib = cpst::getMemoizedObj<FibonacciSolver>(keymaker,serialize,deserialize); //in-memory cache
+  std::cout << memoizedInMemoryFib(3) << std::endl;
 
-  // auto mongoFib = getMongoMemoizedObj<FibSolver>(
-    // fibKeyMaker,intostr,strtoi,"localhost:5000","fib",hashKey
-  // );
-  localFib(6);
-  // mongoFib(6);
-  return 0;
+  // cpst::PersistentMemoized mongoMemoizedFib = cpst::getMongoMemoizedObj<FibonacciSolver>(keymaker,serialize,deserialize,"localhost:5000"); //mongo cache
+  // std::cout << memoizedFib(3) << std::endl;
 }
