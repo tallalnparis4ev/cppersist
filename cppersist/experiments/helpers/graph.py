a143818c3c26a0c41c326758a8447b7a7ea22001
tallalnparis4ev <tm214@st-andrews.ac.uk>
2021-04-15 12:03:13 +0100

    graph script

--- cppersist/experiments/helpers/graph.py
+++ cppersist/experiments/helpers/graph.py
@@ -7,9 +7,9 @@ from os import listdir
 from os.path import isfile, join
 from sigfig import round
 
-should_output = True
-# unit = 1000000000 #Just for Fibonacci
-unit = 1000 #For the rest
+should_output = False
+unit = 1000000000 #Just for Fibonacci
+# unit = 1000 #For the rest
 
 def get_data(file_path):
   if file_path == "data/Fibonacci/WORepRecCpstDelCache.csv":
@@ -23,33 +23,52 @@ def get_data(file_path):
   real_sd = real_col.std()/unit
   return {"real_mean":real_mean, "real_sd":real_sd}
 
-def make_graph(file_paths,save_to):
+def get_name(path):
   names = ['With Cppersist and a\n cache is kept between runs','With Cppersist','Without Cppersist']
-  
+  if path.endswith("NoCpst.csv"):
+    return names[2]
+  elif path.endswith("Cpst.csv"):
+    # return names[0]
+    return names[1]
+  elif path.endswith("CpstDelCache.csv"):
+    return names[1]
+
+def make_graph(file_paths,save_to):
+  # names = ['With Cppersist and a\n cache is kept between runs','With Cppersist','Without Cppersist']
+  names = ['With Cppersist','Without Cppersist']
+  colors = ['green','red'] if len(names) == 2 else ['red', 'blue', 'green']
   plot_dict = {}
   for path in file_paths:
     if path.endswith("NoCpst.csv"):
-      plot_dict[names[2]] = get_data(path)
+      plot_dict[get_name(path)] = get_data(path)
     elif path.endswith("Cpst.csv"):
-      plot_dict[names[0]] = get_data(path)
+      plot_dict[get_name(path)] = get_data(path)
+      pass
     elif path.endswith("CpstDelCache.csv"):
-      plot_dict[names[1]] = get_data(path)
-    
+      plot_dict[get_name(path)] = get_data(path)
   real_means = [plot_dict[name]['real_mean'] for name in names]
+  print("Mean: ",real_means[1]/real_means[0])
   real_std = [plot_dict[name]['real_sd'] for name in names]
+  print(real_means)
+  print(real_std)
   plt.xlabel("Method")
   plt.ylabel("Mean Real Time Taken (s)")
-  bar = plt.bar(names,real_means,alpha=0.5, color=['red', 'blue', 'green'])
+  bar = plt.bar(names,real_means,alpha=0.5, color=colors)
   ytop = [std for i,std in enumerate(real_std)]
-  ybot = [std if real_means[i] - std >= 0 else real_means[0] - 0.0001 for i,std in enumerate(real_std)]
-
+  ybot = [std if real_means[i] - std >= 0 else real_means[i] for i,std in enumerate(real_std)]
+  # stds = [[0,0], real_std]
+  # print(ytop)
+  # print(ybot)
+  # error_bars = plt.errorbar(names,real_means,yerr=(ybot,ytop),capsize=10,fmt='o', markersize=0)
   error_bars = plt.errorbar(names,real_means,yerr=(ybot,ytop) ,capsize=10,fmt='o', markersize=0)
+  bottom, top = plt.ylim()
+  add = (0.005) * (top-bottom)
   for patch in bar.patches:
       label = f'{patch.get_height():.2}'
       if "e" in label:
         label = "{:.20f}".format(float(label)).rstrip('0').rstrip('.')
       # label = "{:.2g}".format(patch.get_height())
-      plt.annotate(label, (patch.get_x() + (0.6 * patch.get_width()), patch.get_height()))
+      plt.annotate(label, (patch.get_x() + (0.6 * patch.get_width()), patch.get_height()+add))
   if should_output:
     plt.savefig(save_to, bbox_inches='tight')
   plt.clf()
@@ -83,14 +102,39 @@ def make_graphs(dir):
 
 
 # make_graphs('cppersist/experiments/data/Primes')
-make_graphs('data/Primes')
+make_graphs('data/TTT')
+
+def manual_graph(save_to):
+  # names = ['With Cppersist and a\n cache is kept between runs','With Cppersist','Without Cppersist']
+  # names = ['With Cppersist','Without Cppersist']
+  names = ['Constructing from serialization','Constructing from dictionary']
+
+  # colors = ['green','red'] if len(names) == 2 else ['red', 'blue', 'green']
+  colors = ['green','red'] if len(names) == 2 else ['red', 'blue', 'green']
+  
+  real_means = [0.2632282333, 0.38528312119999997]
+  real_std = [0.0012436768535279258, 0.0011487550893943892]
+
+  plt.xlabel("Method")
+  plt.ylabel("Mean Real Time Taken (s)")
+  bar = plt.bar(names,real_means,alpha=0.5, color=colors)
+  ytop = [std for i,std in enumerate(real_std)]
+  ybot = [std if real_means[i] - std >= 0 else real_means[0] - 0.0001 for i,std in enumerate(real_std)]
+
+  error_bars = plt.errorbar(names,real_means,yerr=(ybot,ytop) ,capsize=10,fmt='o', markersize=0)
+  for patch in bar.patches:
+      label = f'{patch.get_height():.2}'
+      if "e" in label:
+        label = "{:.20f}".format(float(label)).rstrip('0').rstrip('.')
+      plt.annotate(label, (patch.get_x() + (0.6 * patch.get_width()), patch.get_height()))
+  plt.savefig(save_to, bbox_inches='tight')
+  plt.clf()
+
+# manual_graph('./serialize.png')
 
 
-
-
-
-
-
+# a = pd.read_csv("data/Primes/WORepRecCpstDelCache.csv", header=None).iloc[:,0].squeeze()
+# print(a.mean()/1000)
 
 
 
@@ -107,17 +151,15 @@ make_graphs('data/Primes')
 #          current_dict[_end] = _end
 #      return root
 
-# # Using readlines()
+# # # Using readlines()
 # scrabble = open('words.txt', 'r')
 # lines = scrabble.readlines()
-# trie = make_trie(lines)
+# # trie = make_trie(lines)
 
-# lens = list(map(lambda x : len(x) - 1,lines))
-# a = max(lens)
-# print(a)
-# for x in lens:
-#   if type(x) != int:
-#     print(x)
-# print("hey")
-# print(lens)
-
+# filtered = list(filter(lambda x : len(x)-1 > 4, lines))
+# # lens = list(map(lambda x : len(x) - 1,lines))
+# # a = max(lens)
+# # print(a)
+# # for line in lines:
+#   # if len(line)-1 == 15: 
+#     # print(line)
